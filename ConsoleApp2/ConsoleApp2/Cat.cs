30c865c26a001e700b8c93b6739787a446c2821c
Наталья Добранова <natalia.dobranova@gmail.com>
2019-10-05 15:57:01 +0300

    C#/First

---
+++ ConsoleApp2/ConsoleApp2/Cat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Cat
+    {
+      public string name;
+      public float age;
+      public BreedEnum Breed;
+
+        public Cat(string name, float age, BreedEnum breed = BreedEnum.Brit)
+        {
+            this.name = name;
+            this.age = age;
+            this.Breed = breed;
+           
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.name);
+            sb.AppendLine(this.age.ToString());
+            sb.AppendLine(this.Breed.ToString());
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat("Vaska", 12.6F, BreedEnum.Exotic);
+            Cat cat2 = new Cat("Milka", 3.6F);
+            Cat cat3 = new Cat("Kapa", 10.3F, BreedEnum.None);
+
+            Console.WriteLine(cat1);
+        }
+    }
+
+
+}
+
+
